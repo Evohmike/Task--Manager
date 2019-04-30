293bcd29dc71264cabc4921eabea9dd1f163fa2f
Evohmike <evohmike@gmail.com>
2019-04-30 18:53:53 +0300

    several changes

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    # path('', views.home),
-    path('', views.TaskListView.as_view(), name='task_list'),
+    path('', views.home),
+    path('task_list/', views.TaskListView.as_view(), name='task_list'),
+    path('create/', views.TaskCreateView.as_view(), name='task_create'),
+
 ]
--- task/views.py
+++ task/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView
+from django.utils import timezone
 from .models import Task
 
 # Create your views here.
 
 
-# def home(request):
-#     return HttpResponse("Welcome to simple project Dashboard")
+def home(request):
+    return HttpResponse("Welcome to simple project Dashboard")
 
 
 class TaskListView(ListView):
     model = Task
     context_object_name = 'tasks'
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['task_name', 'task_desc']
+    success_url = '/task_list'
+    extra_context = {'title': 'Create Task'}
+
+    def get_context_data(self, *args, **kwargs):
+        kwargs.update(self.extra_context)
+        return super().get_context_data(*args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.task_creator = self.request.user
+        form.instance.task_created = timezone.now
+        return super().form_valid(form)
